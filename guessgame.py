c044b88e41c981c0a2337e68ab538328573e7985
PRATHAMESH YUVRAJ PAWAR <49082581+prathamesh213@users.noreply.github.com>
2022-06-14 20:58:59 +0530

    Add files via upload

---
+++ guessgame.py
@@ -0,0 +1,20 @@
+
+def game(question, answer):
+    print(question)
+    uans = str(input("what is your answer"))
+    if   uans != answer:
+      print("YOU LOSE SUCKER!!!")
+
+    else:
+        print("your answer is corret you get 10 points")
+
+game("who is the king of the jungle.", "lion")
+game("which animal is the national animal of India", "tiger")
+game("The largest bird alive is the","Ostrich")
+game("The smallest bird alive is the","Hummingbird")
+game("The fastest land animal is the", "Cheetah")
+game("Albatross is a large", "bird")
+game("The largest of all lizards is the","Komodo Dragon")
+game("what does a cow drink","water")
+game("complete the dog breed GOLDEN______", "retriver")
+game("Amber heard is also known as AMBER_____", "turd")
